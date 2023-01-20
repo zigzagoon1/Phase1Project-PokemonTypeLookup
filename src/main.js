59b164c48d33c971f004f81ea22c338f6407153f
KellyMark <kmark602@gmail.com>
2023-01-19 20:28:54 -0500

    updat css, begin early stages of JavaScript development

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+    const form = document.querySelector('#search_form');
+    form.addEventListener('submit', handleSubmit(e));
 })
+
+function handleSubmit(e) {
+
+}
